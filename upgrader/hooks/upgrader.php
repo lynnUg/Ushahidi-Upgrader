8068cf03d6477ab25191bdce6d4a7471df85cab6
Lynn <ntorantyo88@gmail.com>
2012-02-13 00:01:32 +0300

    third commit

---
+++ upgrader/hooks/upgrader.php
@@ -0,0 +1,155 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class upgrader{
+	public $session;
+	public $ftp;
+	public $ftp_server;
+	public $ftp_user_name;
+	public $ftp_user_pass;
+
+	/**
+	 * Registers the main event add method
+	 */
+	public function __construct()
+	{
+		$this->session = Session::instance();
+		// Hook into routing
+		Event::add('system.post_routing', array($this, 'add'));
+	}
+
+	/**
+	 * Adds all the events to the main Ushahidi application
+	 */
+	public function add()
+	{
+		// Only fire if we are in the admin panel
+		$uri_arr = explode('/',Router::$routed_uri);
+		if (in_array('admin',$uri_arr))
+		{
+			Event::add('ushahidi_action.admin_header_top_left', array($this, '_upgrade'));
+			
+		}
+	}
+
+	/**
+	 * Show version
+	 */
+	public function _upgrade()
+	{
+		$view = View::factory('upgrader');
+
+	
+		$view->version_in_config = Kohana::config('version.ushahidi_version');
+
+		// Check if we are upgrading anything
+       
+		if (isset($_POST['versionnotifier_update']))
+		{
+			
+	    
+     		$newbie=dirname(dirname(dirname(dirname(__FILE__))));
+     		//@unlink($newbie."/media/uploads/Ushahidi_Web/application/config/config.php");
+     		//$this->remove_recursively($newbie."/media/uploads/Ushahidi_Web/application/config/config.php");
+			//$this->remove_recursively($newbie."/application/logs");
+			//this code below doesn't work ,the problem is "." directory isn't writable
+			 //$this->copy_recursively($newbie."/media/uploads/Ushahidi_Web",$newbie);
+			//$this->ftp_recursively($newbie."/media/uploads/Ushahidi_Web",$newbie);
+			$this->_process_sql($newbie."/plugins/upgrader/sql_upgrade/");
+			//$this->remove_recursively($newbie."/happy");
+			
+			
+			   
+			
+		}
+
+		
+		if($view->version_in_config < "2.1")
+		{
+			$view->needs_upgrade = TRUE;
+			
+		}else{
+			$view->needs_upgrade = FALSE;
+		}
+		
+
+		$view->render(TRUE);
+	}
+	
+	
+	public function _process_sql($dir_path)
+	{
+	
+		$upgrade_sql = '';
+
+		$files = scandir($dir_path);
+		sort($files);
+		foreach ( $files as $file )
+		{
+			// We're going to try and execute each of the sql files in order
+			$file_ext = strrev(substr(strrev($file),0,4));
+			if ($file_ext == ".sql")
+			{
+				
+				$this->_import_sql($dir_path.$file);
+			}
+		}
+		return "";
+	}
+	public function _import_sql($file2, $table_prefix = NULL)
+	{
+		
+		 $host=Kohana::config('database.default.connection.host');
+		 $username=Kohana::config('database.default.connection.user');
+		 $password=Kohana::config('database.default.connection.pass');
+		 $db_name=Kohana::config('database.default.connection.database');
+		$connection = @mysql_connect("$host", "$username", "$password");
+		if(is_file($file2))
+		{
+			//url::redirect(url::base().'reports');
+		}
+		$db_schema = @file_get_contents($file2);
+
+		// If a table prefix is specified, add it to sql
+		if ($table_prefix) {
+			$find = array(
+				'CREATE TABLE IF NOT EXISTS `',
+				'INSERT INTO `',
+				'ALTER TABLE `',
+				'UPDATE `',
+				'DELETE FROM `'
+				);
+			$replace = array(
+				'CREATE TABLE IF NOT EXISTS `'.$table_prefix.'_',
+				'INSERT INTO `'.$table_prefix.'_',
+				'ALTER TABLE `'.$table_prefix.'_',
+				'UPDATE `'.$table_prefix.'_',
+				'DELETE FROM `'.$table_prefix.'_'
+				);
+			$db_schema = str_replace($find, $replace, $db_schema);
+		}
+
+		
+		@mysql_select_db($db_name,$connection);
+		/**
+		 * split by ; to get the sql statement for creating individual
+		 * tables.
+		 */
+		$tables = explode(';',$db_schema);
+
+		foreach($tables as $query) {
+
+			$result = @mysql_query($query,$connection);
+		}
+
+		@mysql_close( $connection );
+
+	}
+	
+	public function _do_upgrade() 
+	{
+       
+	}
+	
+}
+
+new upgrader;
---
+++ upgrader/libraries/Loader.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * Upgrading  Library
+ * Provides the necessary functions to do the automatic upgrade
+ * 
+ * @package    Upgrade
+ * @author     Ushahidi Team
+ * @copyright  (c) 2008 Ushahidi Team
+ * @license    http://www.ushahidi.com/license.html
+ */
+ 
+ ?>
